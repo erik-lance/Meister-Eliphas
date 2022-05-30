a169c7bf1035d8d8f3cbe671475c6460b3728714
erik-lance <eriklance@gmail.com>
2022-05-30 13:51:36 +0800

    HP halved, added attacker on player's base
    Eliphas now targets knights attacking the base

--- assets/scripts/base.gd
+++ assets/scripts/base.gd
@@ -11,7 +11,10 @@ var game_manager
 var base_owner
 var cur_enemy
 
-var hp = 3000
+var max_hp = 1500
+var hp = max_hp
+
+var dm = 20
 
 var target_list = []
 
@@ -28,34 +31,60 @@ func _ready():
 func _process(delta):
 	if (target_list.size() > 0):
 		
-		if (target_list[0].get_parent() != null):
-			gal_ai.force_y(target_list[0].get_parent().position.y)	
-			# Checks if current target is dead
-			if (target_list[0].get_parent().cur_state == 3):
-				target_list.pop_front()
+		if (base_owner == Owner.AI):
+			if (target_list[0].get_parent() != null):
+				gal_ai.force_y(target_list[0].get_parent().position.y)	
+				# Checks if current target is dead
+				if (target_list[0].get_parent().cur_state == 3):
+					target_list.pop_front()
+					gal_ai.force_y(null)
+			else:
 				gal_ai.force_y(null)
-		else:
-			gal_ai.force_y(null)
+		elif (base_owner == Owner.PLAYER && (cur_state != Status.DEAD)):
+			if (cur_state == Status.ATTACK):
+				game_manager.eliphas.play("point")
+
+func get_target():
+	if (target_list.size() != 0): return target_list[0]
+	else: return null
+
+func switch_target():
+	cur_enemy = get_target()
+	if cur_enemy != null: cur_state = Status.ATTACK
+	else: cur_state = Status.IDLE
+
+func defeated_target():
+	target_list.pop_front()
+	switch_target()
+
+func inflict_damage():
+	var defeated = false
+	defeated = cur_enemy.get_parent().take_damage(dm)
+	# Sends a signal to data to remove from target list
+	if(defeated): defeated_target()
 
 func take_damage(d):
 	hp -= d;
 	var hp_state
 	
-	if (hp > 3000*.75): hp_state = 0
-	elif (hp > 3000*.50): hp_state = 1
-	elif (hp > 3000*.25): hp_state = 2
+	if (hp > max_hp*.75): hp_state = 0
+	elif (hp > max_hp*.50): hp_state = 1
+	elif (hp > max_hp*.25): hp_state = 2
 	else: hp_state = 3
 	
 	if (base_owner == Owner.AI):
 		game_manager.update_health_enemy(hp, hp_state)
-		if (hp < 3000 *.50): gal_ai.harden()
+		if (hp < max_hp *.50): gal_ai.harden()
 	else:
 		game_manager.update_health_player(hp, hp_state)
-		if (hp < 3000 *.50): game_manager.main_ui.ease()
+		if (hp < max_hp *.50): game_manager.main_ui.ease_game()
 	
 	
 	
 	if (hp <= 0):
+		if (base_owner == Owner.PLAYER): game_manager.base_dead(false)
+		else: game_manager.base_dead(true)
+		
 		audio_player.stream = sound_dead
 		audio_player.play()
 		cur_state = Status.DEAD;
@@ -68,4 +97,4 @@ func take_damage(d):
 # Only  for enemy base AI
 func _on_Area2D_body_entered(body):
 	target_list.append(body)
-	
+	if (base_owner == Owner.PLAYER && target_list.size()==1): switch_target()
