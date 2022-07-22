7e9b4827459c7e60bacece7cb35809622c4c3d3b
erik-lance <eriklance@gmail.com>
2022-07-22 12:51:27 +0800

    Sliders now update based on current bus value

--- assets/scripts/settings.gd
+++ assets/scripts/settings.gd
@@ -1,15 +1,14 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var master_s = $CanvasLayer/VBoxContainer/master_slider
+onready var music_s = $CanvasLayer/VBoxContainer/music_slider
+onready var sfx_s = $CanvasLayer/VBoxContainer/sfx_slider
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	master_s.value = AudioServer.get_bus_volume_db(0)
+	music_s.value = AudioServer.get_bus_volume_db(1)
+	sfx_s.value = AudioServer.get_bus_volume_db(2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
